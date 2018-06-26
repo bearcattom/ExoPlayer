f312971e1a1eac0052037c97cc116bb819759bbf
Tom Schultz <tom.schultz@fox.com>
2018-06-26 13:02:08 -0400

    Updated Matches

--- demos/main/src/main/java/com/foxsports/worldcup/streamtest/SampleChooserActivity.java
+++ demos/main/src/main/java/com/foxsports/worldcup/streamtest/SampleChooserActivity.java
@@ -60,13 +60,10 @@ import java.time.format.DateTimeFormatter;
 import java.time.ZonedDateTime;
 import java.util.TimeZone;
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.AuthFailureError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
